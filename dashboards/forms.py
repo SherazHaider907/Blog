7eb5cea5de07206999a31fec2bae01a9a57135d7
SherazHaider907 <Sherazhaider665@gmail.com>
2026-01-06 14:49:47 +0500

    manage users functionality for managers

--- dashboards/forms.py
+++ dashboards/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 
 from blogs.models import Blog, Catagory
+from django.contrib.auth.forms import UserChangeForm
+
+from django.contrib.auth.models import User
 
 class CatagoryForm(forms.ModelForm):
     class Meta:
@@ -11,3 +14,14 @@ class BlogPostForm(forms.ModelForm):
     class Meta:
         model = Blog
         fields = ['title', 'catagory', 'featured_image', 'short_description', 'blog_body', 'status', 'is_featured']
+
+
+class AddUserForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'is_active', 'is_staff', 'is_superuser','groups','user_permissions']
+    
+class EditUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'is_active', 'is_staff', 'is_superuser','groups','user_permissions']
